d9ad4a47bdbcb49eea17bad0d55717f8553cd50d
winniediana4 <deinawinnie84@gmail.com>
2021-05-24 19:37:54 +0300

    generate component repository

--- GSearch/src/app/app.module.ts
+++ GSearch/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RepositoryComponent } from './repository/repository.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepositoryComponent
   ],
   imports: [
     BrowserModule,
